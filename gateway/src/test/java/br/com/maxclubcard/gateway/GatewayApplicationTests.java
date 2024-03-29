aaa975ba33d2eb00d39a91216f6d6841fc671853
jeronima <florianojeronima@gmail.com>
2024-03-29 15:05:21 -0300

    feat: Adicionando o serviço Gateway da aplicação

---
+++ gateway/src/test/java/br/com/maxclubcard/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maxclubcard.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
