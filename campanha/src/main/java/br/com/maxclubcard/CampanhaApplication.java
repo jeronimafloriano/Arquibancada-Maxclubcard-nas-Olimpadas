7585a5a60a340e685e60ebce492720da14447cac
jeronima <florianojeronima@gmail.com>
2024-03-29 20:42:43 -0300

    fix: Movendo a classe CampanhaApplication para raiz do pacote.

--- campanha/src/main/java/br/com/maxclubcard/CampanhaApplication.java
+++ campanha/src/main/java/br/com/maxclubcard/CampanhaApplication.java
@@ -1,11 +1,13 @@
-package br.com.maxclubcard.campanha;
+package br.com.maxclubcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableWebMvc
 public class CampanhaApplication {
 
 	public static void main(String[] args) {
