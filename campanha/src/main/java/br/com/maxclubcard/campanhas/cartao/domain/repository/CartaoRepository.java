0b4b6b9d93d7bc142b9669a73ac63c4884889f1e
jeronima <florianojeronima@gmail.com>
2024-04-02 12:32:23 -0300

    feat: Adicionando no repository método para validação de cartão já existente

--- campanha/src/main/java/br/com/maxclubcard/campanhas/cartao/domain/repository/CartaoRepository.java
+++ campanha/src/main/java/br/com/maxclubcard/campanhas/cartao/domain/repository/CartaoRepository.java
@@ -1,8 +1,11 @@
 package br.com.maxclubcard.campanhas.cartao.domain.repository;
 
+import br.com.maxclubcard.campanhas.cartao.domain.Bandeira;
 import br.com.maxclubcard.campanhas.cartao.domain.Cartao;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartaoRepository extends JpaRepository<Cartao, Long> {
 
+  Optional<Cartao> findByNumeroAndBandeira(String numero, Bandeira bandeira);
 }
