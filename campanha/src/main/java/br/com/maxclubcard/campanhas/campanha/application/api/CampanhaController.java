033ce9d48b5f597d89b95161e361a082bd1d1e17
jeronima <florianojeronima@gmail.com>
2024-04-02 12:26:40 -0300

    fix: Retirando endpoint da porta que foi usado para testes

--- campanha/src/main/java/br/com/maxclubcard/campanhas/campanha/application/api/CampanhaController.java
+++ campanha/src/main/java/br/com/maxclubcard/campanhas/campanha/application/api/CampanhaController.java
@@ -10,7 +10,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import java.util.List;
 import javax.validation.Valid;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,16 +23,6 @@ public class CampanhaController {
 
   public CampanhaController(CampanhaService campanhaService) {
     this.campanhaService = campanhaService;
-  }
-
-  @Operation(
-      description = "Retorna a porta que foi indicada para rodar a aplicação",
-      tags = {"Campanha"})
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Porta encontrada com sucesso")})
-  @GetMapping("/porta")
-  public String retornaPorta(@Value("${local.server.port}") String porta) {
-    return String.format("Requisição respondida pela instância executando na porta %s", porta);
   }
 
   @Operation(
