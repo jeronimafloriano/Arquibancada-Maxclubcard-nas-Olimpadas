c91003f4a4ea6336343d90b81c031e2849b26349
jeronima <florianojeronima@gmail.com>
2024-04-02 12:30:07 -0300

    feat: Adicionando dados para testes

--- campanha/src/test/java/br/com/maxclubcard/campanhas/ApplicationFactoryTests.java
+++ campanha/src/test/java/br/com/maxclubcard/campanhas/ApplicationFactoryTests.java
@@ -1,9 +1,11 @@
 package br.com.maxclubcard.campanhas;
 
+import br.com.maxclubcard.campanhas.campanha.domain.Campanha;
 import br.com.maxclubcard.campanhas.cartao.domain.Cartao;
 import br.com.maxclubcard.campanhas.cliente.domain.Cliente;
 import br.com.maxclubcard.campanhas.cliente.domain.Email;
 import br.com.maxclubcard.campanhas.cliente.domain.Sexo;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 public class ApplicationFactoryTests {
@@ -15,6 +17,10 @@ public class ApplicationFactoryTests {
 
   public static Cliente umCliente() {
     return new Cliente(nome, cpf, dataNascimento, Sexo.FEMININO, email, "(62) 91234-5098");
+  }
+
+  public static Campanha umaCampanha() {
+    return new Campanha("MaxClubCard", BigDecimal.valueOf(150));
   }
 
 }
